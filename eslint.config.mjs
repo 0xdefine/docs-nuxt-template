46d49388f9da50b79be446d872d4bd650be6dfc5
Sabrina Ferguson <sf@matterlabs.dev>
2024-05-13 15:05:35 -0400

    chore: add comment to eslintconfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs';
 
+/**
+ * To inspect the full eslint config, run the following:
+ * bunx @eslint/config-inspector
+ */
 export default withNuxt(
   {
     rules: {
