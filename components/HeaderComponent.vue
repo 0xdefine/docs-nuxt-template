2ab0b15d184752a022478d3475df8398b91c9069
Sabrina Ferguson <sf@matterlabs.dev>
2024-05-14 14:19:53 -0400

    chore: cleanup

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -1,10 +1,5 @@
 <script setup lang="ts">
 import type { NavItem } from '@nuxt/content/dist/runtime/types';
-import type { Link } from '@nuxt/ui-pro/types';
-
-defineProps<{
-  links: Link[];
-}>();
 
 const navigation = inject<NavItem[]>('navigation', []);
 
